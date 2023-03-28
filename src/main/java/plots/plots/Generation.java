fbc91b9e1acd63f937c7301a7c4841701c97baf4
rayen <rayenrathmann@gmail.com>
2023-03-28 20:25:48 +0200

    Added based files

---
+++ src/main/java/plots/plots/Generation.java
@@ -0,0 +1,4 @@
+package plots.plots;
+
+public class Generation {
+}
---
+++ src/main/java/plots/plots/Plots.java
@@ -0,0 +1,17 @@
+package plots.plots;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plots extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
